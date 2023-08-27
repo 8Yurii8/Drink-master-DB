e7cc998c68e25a75f634365a589cf0151b1970e7
Natali <114168847+Natali9108@users.noreply.github.com>
2023-08-27 20:55:46 +0300

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -1,9 +1,5 @@
 export { default as recipesSchema } from "./recipesSchema.js";
 export { default as ingredientsSchema } from "./ingredientsSchema.js";
 export { default as glassesSchema } from "./glassesSchema.js";
-export { default as categoriesSchema } from "./categoriesSchema.js";
-export { default as userSchema } from "./userSchema.js";
-export { default as recipesSchema } from "./recipesSchema.js";
-export { default as ingredientsSchema } from "./ingredientsSchema.js";
 export { default as userSchema } from "./userSchema.js";
 export { default as emailSchema } from "./emailSchema.js";
