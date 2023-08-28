57332f593e95c0fc81e5ca734efeb97922824153
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-08-28 21:35:01 +0300

    fix swagger

--- swagger/index.js
+++ swagger/index.js
@@ -1,7 +1,10 @@
+import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const swaggerPath = path.join(__dirname, "swagger.json");
 const swaggerRaw = fs.readFileSync(swaggerPath, "utf8");
 const swaggerDocument = JSON.parse(swaggerRaw);
