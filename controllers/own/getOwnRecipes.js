307db2d319fde2ed364257484e1e06097e4cc8af
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-09-01 23:44:26 +0300

    fix own pagination

--- controllers/own/getOwnRecipes.js
+++ controllers/own/getOwnRecipes.js
@@ -3,12 +3,7 @@ import { ctrlWrapper } from "../../helpers/index.js";
 
 const getOwnRecipes = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
-  const search = await Recipes.find({ owner: [_id] }, " ", {
-    skip,
-    limit,
-  });
+  const search = await Recipes.find({ owner: [_id] });
 
   res.json(search);
 };
