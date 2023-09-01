f190c3e7121204d1a32f4ce6dea88cf51174175d
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-09-01 21:36:14 +0300

    fix: add and remove favorite

---
+++ controllers/favorite/addToFavorite.js
@@ -0,0 +1,27 @@
+import Recipes from "../../models/recipes.js";
+import { ctrlWrapper } from "../../helpers/index.js";
+
+const addToFavorite = async (req, res) => {
+  const { _id } = req.user;
+  const userId = _id.toString();
+  const recipeId = req.params.id;
+
+  try {
+    let recipe = await Recipes.findById(recipeId);
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+    recipe = await Recipes.findByIdAndUpdate(
+      recipeId,
+      { $addToSet: { favorites: userId } },
+      { new: true, upsert: true }
+    );
+    const favorites = recipe.favorites;
+    res.json({ favorites, message: "Recipe added to favorite" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
+
+export default ctrlWrapper(addToFavorite);
