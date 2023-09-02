1c64112d63f3fc6ad5056ca3ce5fadf5ffab6d01
Danil Takiylin <danadanil900@gmail.com>
2023-09-02 12:39:38 +0200

    refactor: delete pagination

--- controllers/favorite/getFavorite.js
+++ controllers/favorite/getFavorite.js
@@ -5,22 +5,9 @@ import Recipes from "../../models/recipes.js";
 const getFavoriteDrinks = async (req, res) => {
   const { id } = req.user;
 
-  const { page = 1, limit = 9 } = req.query;
+  const allRecipe = await Recipes.find({ favorites: id });
 
-  const qty = page * limit;
-
-  const allRecipe = await Recipes.find(
-    { favorites: [id] },
-    "-createdAd, -updateAd",
-    {
-      skip: 0,
-      limit: qty,
-    }
-  );
-
-  const totalHitsRecipe = await Recipes.find({ favorites: [id] });
-
-  res.json({ favorites: allRecipe, totalHits: totalHitsRecipe.length });
+  res.json(allRecipe);
 };
 
 export default ctrlWrapper(getFavoriteDrinks);
