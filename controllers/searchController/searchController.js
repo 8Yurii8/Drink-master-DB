28eee85ea043f0e44171c466d9a5b6764f226edd
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-09-02 22:17:11 +0300

    fix search

--- controllers/searchController/searchController.js
+++ controllers/searchController/searchController.js
@@ -2,7 +2,7 @@ import { Recipes } from "../../models/index.js";
 import { ctrlWrapper } from "../../helpers/index.js";
 
 const searchController = async (req, res) => {
-  const { drink, category, ingredients } = req.body;
+  const { drink, category, ingredients } = req.query;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
 
@@ -23,46 +23,15 @@ const searchController = async (req, res) => {
 
   const search = await Recipes.find(searchConditions).skip(skip).limit(limit);
 
-  res.json(search);
+  const totalResults = await Recipes.countDocuments(searchConditions);
+
+  const totalPages = Math.ceil(totalResults / limit);
+
+  res.json({
+    results: search,
+    totalResults,
+    totalPages,
+  });
 };
+
 export default ctrlWrapper(searchController);
-
-// const searchController = async (req, res) => {
-//   const { drink, category, ingredients } = req.body;
-//   const { page = 1, limit = 10 } = req.query;
-//   const skip = (page - 1) * limit;
-//   const search = await Recipes.find({}, " ", {
-//     skip,
-//     limit,
-//   });
-
-//   let filter = search;
-
-//   if (drink) {
-//     const regex = new RegExp(drink, "i");
-//     filter = filter.filter(
-//       (recepi) => recepi._doc.drink && regex.test(recepi._doc.drink)
-//     );
-//   }
-
-//   if (category) {
-//     filter = filter.filter(
-//       (recepi) =>
-//         recepi._doc.category && recepi._doc.category.includes(category)
-//     );
-//   }
-
-//   if (ingredients) {
-//     filter = filter.filter(
-//       (recepi) =>
-//         recepi._doc.ingredients &&
-//         recepi._doc.ingredients.some(
-//           (ingredient) => ingredient.title === ingredients
-//         )
-//     );
-//   }
-
-//   res.json(filter);
-// };
-
-// export default ctrlWrapper(searchController);
