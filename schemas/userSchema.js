08923c45ab0356118d3686ca92b99ba8c8bbb06f
Ruslan <Giovingo92@gmail.com>
2023-09-02 17:47:41 +0300

    feat: add validation for signin

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -3,11 +3,12 @@ import { emailRegexp, passwordUppercase } from "./regexp.js";
 
 const userSignUpSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required().pattern(emailRegexp),
+  email: Joi.string().pattern(emailRegexp),
   password: Joi.string().pattern(passwordUppercase).min(6).max(16).required(),
 });
 
 const userSignInSchema = Joi.object({
+  name: Joi.string(),
   email: Joi.string().required().pattern(emailRegexp),
   password: Joi.string().pattern(passwordUppercase).min(6).max(16).required(),
 });
