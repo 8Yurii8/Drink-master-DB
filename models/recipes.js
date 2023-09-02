f05f71a2f80a759e0ab91baee1e27eb7f55db98b
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-09-02 12:59:10 +0300

    fix model

--- models/recipes.js
+++ models/recipes.js
@@ -1,96 +1,96 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 const ingredientsSchema = new Schema({
-    title: { type: String, required: true },
-    measure: { type: String, required: true },
-    ingredientThumb: { type: String, required: true },
-    'thumb-medium': { type: String, required: true },
-    'thumb-small': { type: String, required: true },
+  title: { type: String, required: true },
+  measure: { type: String, required: true },
+  ingredientThumb: { type: String, required: true },
+  "thumb-medium": { type: String, required: true },
+  "thumb-small": { type: String, required: true },
 });
 
 const schema = new Schema({
-    drink: {
-        type: String,
-        required: true,
-    },
-    imageThumb: {
-        type: String,
-        required: true,
-    },
-    drinkAlternate: {
-        type: String,
-        default: '',
-    },
-    tags: {
-        type: [String],
-        default: '',
-    },
-    video: {
-        type: String,
-        default: '',
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    IBA: {
-        type: String,
-        default: '',
-    },
-    alcoholic: {
-        type: String,
-        default: '',
-    },
-    glass: {
-        type: String,
-        required: true,
-    },
-    instructions: {
-        type: [String],
-        required: true,
-    },
-    instructionsES: {
-        type: [String],
-        default: '',
-    },
-    instructionsDE: {
-        type: [String],
-        default: '',
-    },
-    instructionsFR: {
-        type: [String],
-        default: '',
-    },
-    instructionsIT: {
-        type: [String],
-        default: '',
-    },
-    instructionsRU: {
-        type: [String],
-        default: '',
-    },
-    instructionsPL: {
-        type: [String],
-        default: '',
-    },
-    instructionsUK: {
-        type: [String],
-        default: '',
-    },
-    ingredients: {
-        type: [ingredientsSchema],
-        required: true,
-    },
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-    },
-    favorites: {
-        type: [Schema.Types.ObjectId],
-        ref: 'user',
-    },
+  drink: {
+    type: String,
+    required: true,
+  },
+  imageThumb: {
+    type: String,
+    required: true,
+  },
+  drinkAlternate: {
+    type: String,
+    default: "",
+  },
+  tags: {
+    type: [String],
+    default: "",
+  },
+  video: {
+    type: String,
+    default: "",
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  IBA: {
+    type: String,
+    default: "",
+  },
+  alcoholic: {
+    type: String,
+    default: "",
+  },
+  glass: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: [String],
+    required: true,
+  },
+  instructionsES: {
+    type: [String],
+    default: "",
+  },
+  instructionsDE: {
+    type: [String],
+    default: "",
+  },
+  instructionsFR: {
+    type: [String],
+    default: "",
+  },
+  instructionsIT: {
+    type: [String],
+    default: "",
+  },
+  instructionsRU: {
+    type: [String],
+    default: "",
+  },
+  instructionsPL: {
+    type: [String],
+    default: "",
+  },
+  instructionsUK: {
+    type: [String],
+    default: "",
+  },
+  ingredients: {
+    type: [ingredientsSchema],
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  favorites: {
+    type: [String],
+    ref: "user",
+  },
 });
 
-const Recipes = model('recipes', schema);
+const Recipes = model("recipes", schema);
 
 export default Recipes;
