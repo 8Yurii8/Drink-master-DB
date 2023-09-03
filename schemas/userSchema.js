d6b4a07377f4815707e2de6692f8b373538f4580
Ruslan <Giovingo92@gmail.com>
2023-09-03 23:24:56 +0300

    fix: add maessage

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -2,14 +2,49 @@ import Joi from "joi";
 import { emailRegexp, passwordUppercase } from "./regexp.js";
 
 const userSignUpSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required().pattern(emailRegexp),
-  password: Joi.string().pattern(passwordUppercase).min(6).max(16).required(),
+  name: Joi.string().required().messages({
+    "any.required": "Name is required.",
+    "string.empty": "Name cannot be empty.",
+  }),
+  email: Joi.string().required().pattern(emailRegexp).messages({
+    "any.required": "Email is required.",
+    "string.empty": "Email cannot be empty.",
+    "string.pattern.base": "Email is not in a valid format.",
+  }),
+  password: Joi.string()
+    .pattern(passwordUppercase)
+    .min(6)
+    .max(16)
+    .required()
+    .messages({
+      "any.required": "Password is required.",
+      "string.empty": "Password cannot be empty.",
+      "string.min": "Password must be at least {#limit} characters long.",
+      "string.max": "Password cannot be longer than {#limit} characters.",
+      "string.pattern.base":
+        "Password must contain at least one uppercase letter.",
+    }),
 });
 
 const userSignInSchema = Joi.object({
-  email: Joi.string().required().pattern(emailRegexp),
-  password: Joi.string().pattern(passwordUppercase).min(6).max(16).required(),
+  email: Joi.string().required().pattern(emailRegexp).messages({
+    "any.required": "Email is required.",
+    "string.empty": "Email cannot be empty.",
+    "string.pattern.base": "Email is not in a valid format.",
+  }),
+  password: Joi.string()
+    .pattern(passwordUppercase)
+    .min(6)
+    .max(16)
+    .required()
+    .messages({
+      "any.required": "Password is required.",
+      "string.empty": "Password cannot be empty.",
+      "string.min": "Password must be at least {#limit} characters long.",
+      "string.max": "Password cannot be longer than {#limit} characters.",
+      "string.pattern.base":
+        "Password must contain at least one uppercase letter.",
+    }),
 });
 
 export default {
