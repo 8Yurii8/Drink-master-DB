221d06643568fb92f29c035f26de5e5f6cbf5784
Yurii <114691896+8Yurii8@users.noreply.github.com>
2023-09-04 00:02:03 +0300

    Update getFavorite.js

--- controllers/favorite/getFavorite.js
+++ controllers/favorite/getFavorite.js
@@ -4,16 +4,11 @@ import Recipes from "../../models/recipes.js";
 const getFavoriteDrinks = async (req, res) => {
   const { id } = req.user;
 
-  try {
-    const allRecipe = await Recipes.find({
+  const allRecipe = await Recipes.find({
       "favorites.userId": id,
-    });
 
     res.json(allRecipe);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+  
 };
 
 export default ctrlWrapper(getFavoriteDrinks);
